bad2f21138557f026df367b3969608098d4400a8
bryanherger <bryanherger@gmail.com>
2020-05-27 11:17:45 -0400

    Delete xmlparser.sql
    removed outdated SQL file

--- xmlparser.sql
+++
@@ -1,9 +0,0 @@
-ALTER DATABASE docker SET JavaBinaryForUDx = '/usr/java/latest/bin/java';
-
-CREATE OR REPLACE LIBRARY verticajavaudl AS '/tmp/vertica-java-udl-0.1-jar-with-dependencies.jar' language 'java';
-
-CREATE OR REPLACE PARSER XmlParser AS LANGUAGE 'java' NAME 'XmlParserFactory' LIBRARY verticajavaudl;
-
-CREATE TABLE public.examplexml (tag1 VARCHAR, tag2 VARCHAR);
-
-COPY public.examplexml FROM '/tmp/example.xml' PARSER XmlParser();
